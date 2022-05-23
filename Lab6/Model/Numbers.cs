ede2fe12f3bbad3488cc33aa2592ce436a0a28a3
НиколайЫ ЗенковЫ <zzzkolya007@bk.ru>
2022-05-23 12:50:55 +1000

    Добавьте файлы проекта.

---
+++ Lab6/Model/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using Lab6.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab6.Model
+{
+    class Numbers
+    {
+        List<int> temp = new List<int>();
+        public int returnResult(int value)
+        {
+            int res = 0;
+            for(int i = 0; i <= value; ++i)
+            {
+                temp.Add(i);
+            }
+
+            for(int i = 0; i <= value; ++i)
+            {
+                res += temp[i];
+            }
+
+            return res;
+        }
+
+    }
+}
---
+++ Lab6/ViewModels/MyViewModel.cs
@@ -0,0 +1,107 @@
+﻿using Lab6.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lab6.ViewModels
+{
+    class MyViewModel : INotifyPropertyChanged
+    {
+        private int result;    //Результат умножения. Обратите внимание, что все поля приватные
+        private int result1;    //Результат умножения. Обратите внимание, что все поля приватные
+        private int selectedValue; 
+
+       
+        public int Result
+        {
+            get
+            {
+                return result;
+            }
+
+            set
+            {
+                result = value;
+                RaisePropertyChanged("Result");
+            }
+        }
+        public int Result1
+        {
+            get
+            {
+                return result1;
+            }
+
+            set
+            {
+                result1 = value;
+                RaisePropertyChanged("Result");
+            }
+        }
+
+        public int ValueScroll
+        {
+
+            get
+            {
+                return selectedValue;
+            }
+
+            set
+            {
+                selectedValue = value;
+                RaisePropertyChanged("ValueScroll");
+            }
+        }
+
+        
+
+        private RelayCommand clickCommand;
+        public RelayCommand ClickCommand
+        {
+            get
+            {
+                return clickCommand ??
+                  (clickCommand = new RelayCommand(obj =>
+                  {  //при нажатии на кнопку производится расчет результата умножения
+                      Numbers numbers = new Numbers();  //создадим объект класса модели
+                      Result = numbers.returnResult(ValueScroll); //вызовем функцию расчета результата и присвоим ее значение
+                      //соответствующему полю Result
+                  }));
+            }
+        }
+
+
+        private RelayCommand resetCommand;
+        public RelayCommand ResetCommand
+        {
+            get
+            {
+                return resetCommand ??
+                  (resetCommand = new RelayCommand(obj =>
+                  {
+                      Result = 0;
+                      ValueScroll = 0;
+                  }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+
+
+        public MyViewModel() //пустой конструктор
+        {
+        }
+    }
+}
